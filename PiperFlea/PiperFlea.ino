cd1bbbffb98e67ccf7f5c0692599f4d9a3043b59
Andreas Rau <andreas.rau@student.hpi.uni-potsdam.de>
2014-10-22 00:29:59 +0200

    switched blue and red for video

--- PiperFlea/PiperFlea.ino
+++ PiperFlea/PiperFlea.ino
@@ -32,8 +32,8 @@ rFlea_Arduino rflea = rFlea_Arduino();
 unsigned int serialNumber;
 
 int led = 13;
-int redPin = 3;
-int bluePin = 11;
+int redPin = 11;
+int bluePin = 3;
 int greenPin = 10;
 
 int basePressure;
