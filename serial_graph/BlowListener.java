566d921cff91f7f22208649f16357758dd1a1a80
Andreas Rau <andreas.rau@student.hpi.uni-potsdam.de>
2014-10-22 00:31:22 +0200

    cleaned up

--- serial_graph/BlowListener.java
+++
@@ -1,8 +0,0 @@
-interface BlowListener{
- public abstract void puff(); 
- public abstract void sip();
- public abstract void longBlowStart();
- public abstract void longBlowStop();
- public abstract void longSuckStart();
- public abstract void longSuckStop();
-}
